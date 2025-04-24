df04f541b0c7bd75489a6c2318417ac8a977cc1f
Shubhada Martha <103222835+smartha2003@users.noreply.github.com>
2025-04-23 18:08:53 -0700

    Fix Tailwind purge config for top-level App.tsx

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}', "./App.tsx"],
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+    './App.tsx',             // ✅ top-level App file
+    './src/**/**/*.{js,ts,jsx,tsx}' // ✅ any nested components
+  ],
   theme: {
     extend: {},
   },
